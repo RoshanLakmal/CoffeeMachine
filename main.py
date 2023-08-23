ddffc5b7f5fa8d71580782988323f1d34fda6f52
Roshan <roshanrox911@gmail.com>
2023-08-23 22:28:58 +1000

    added check_availability

--- main.py
+++ main.py
@@ -30,15 +30,34 @@ resources = {
     "coffee": 100,
 }
 
+require_resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+total_money = 0
+
+def is_resources_available(require_resources):
+    is_res_available = True
+    for res in resources:
+        if require_resources[res]>resources[res]:
+            is_res_available = False
+            print(f"Sorry there is not enough {res}.")
+    return is_res_available
 
 def user_commands(user_input):
     match user_input:
         case "off":
             exit()
         case "report":
-            return "Not found"
-        # case 418:
-        #     return "I'm a teapot"
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${total_money}")
+        case "check_availability":
+            print(is_resources_available(require_resources))
+            # return "I'm a teapot"
 
         # If an exact match is not confirmed, this last case will be used if provided
         # case _:
@@ -46,8 +65,9 @@ def user_commands(user_input):
 
 
 def start_coffee_machine():
-    user_coffe_choice = input("What would you like? (espresso/latte/cappuccino): ")
+    # user_coffe_choice = input("What would you like? (espresso/latte/cappuccino): ")
+    user_coffe_choice = "check_availability"
     user_commands(user_coffe_choice)
-    start_coffee_machine()
+    # start_coffee_machine()
 
 start_coffee_machine()
